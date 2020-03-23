973ff560d351c055ec933cd8519a53047dcc1694
hemanthsagar <47024942+hemanthsagar@users.noreply.github.com>
2020-03-23 12:07:11 +0530

    firstcommit of this repository

---
+++ delete_A_if_ir_occurs_at_starting_two_positions/src/main/java/org/example/App.java
@@ -0,0 +1,18 @@
+package org.example;
+
+
+import java.util.Scanner;
+
+public class App
+{
+    public static void main( String[] args )
+    {
+        Scanner sc=new Scanner(System.in);
+        while(true){
+        System.out.println("Enter a String");
+        String s=sc.next();
+        Delete_cap_a obj1=new Delete_cap_a();
+        s=obj1.delete_cap_a(s,1,'A');
+        System.out.println(s);}
+    }
+}
